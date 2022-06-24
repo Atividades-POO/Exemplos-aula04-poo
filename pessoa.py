50044c2607708626bb0eeb9b26df9e7c55a7ec1d
Michel <106674127+MichelZero@users.noreply.github.com>
2022-06-24 09:59:48 -0300

    Create pessoa.py

---
+++ pessoa.py
@@ -0,0 +1,23 @@
+#
+# criar a classe pessoa
+class Pessoa:
+  def __init__(self, nome, login = False, logoff = False): # inicializador
+    self.nome = nome # atributo
+    self.login = login
+    self.logoff = logoff
+
+  def logar(self):
+    if self.login:
+      print(f"{self.nome} já está logado no sistema!")
+      return
+    print(f"Bem vindo {self.nome}, você logou no sistema.")
+    self.login = True
+    self.logoff = False
+
+  def sair(self):
+    if not self.login:
+      print(f"{self.nome} não está logado no sistema!")
+      return
+    print(f"{self.nome}, você saiu do sistema.")
+    self.login = False
+    self.logoff = True
