519fb992c99285da8f4a8f24b7b30a30bb4cf05c
Michel <106674127+MichelZero@users.noreply.github.com>
2022-06-24 12:58:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -73,13 +73,13 @@ print()
 from b import B
 # 3.1 crie uma instância da classe b
 b1 = B(10) # cria um objeto da classe b com valor 10
-# 3.2 atribua um valor ao atributo _valor
-b1.b = 20 # atribui o valor 20 ao atributo _valor
-# 3.3 atribua um valor ao atributo __valor2
-b1.b2 = 30 # atribui o valor 30 ao atributo __valor2
-# 3.4 veja o valor do atributo _valor
+# 3.2 atribua um valor ao atributo _b
+b1.b = 20 # atribui o valor 20 ao atributo _b
+# 3.3 atribua um valor ao atributo __b2
+b1.b2 = 30 # atribui o valor 30 ao atributo __b2
+# 3.4 veja o valor do atributo _b
 print(b1.b) # vai imprimir 20
-# 3.5 veja o valor do atributo __valor2
+# 3.5 veja o valor do atributo __b2
 print(b1.b2) # vai imprimir 30
 
 
