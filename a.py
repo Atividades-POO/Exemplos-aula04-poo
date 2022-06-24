04f4467f496d5ae1762f114c6b09372e2afbd288
Michel <106674127+MichelZero@users.noreply.github.com>
2022-06-24 11:05:31 -0300

    Update a.py

--- a.py
+++ a.py
@@ -2,12 +2,19 @@
 # 2 - criar a class a
 class A:
   def __init__(self, valor): # inicializador
-    self._valor = valor # atributo
+    self._valor = valor # atributo privado
+    self.__valor2 = valor # atributo protegido
 
   # get
   def get_valor(self): # método de instância
-    return self._valor  # Retorna o valor da string  informada
+    return self._valor  # Retorna o conteudo de _valor
+
+  def get_valor2(self): # método de instância
+    return self.__valor2  # Retorna o valor de __valor2
 
   # set
-  def set_valor(self, valor):
-    self._valor = valor # Atribui o valor ao atributo
+  def set_valor(self, valor): # seta o valor do atributo _valor
+    self._valor = valor # Atribui o valor ao atributo _valor
+
+  def set_valor2(self, valor): # seta o valor do atributo __valor2
+    self.__valor2 = valor # Atribui o valor ao atributo __valor2
