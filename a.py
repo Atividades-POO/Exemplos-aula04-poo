8377d854d0aa29e766bdf6f28ba55a653869c75f
Michel <106674127+MichelZero@users.noreply.github.com>
2022-06-24 10:12:17 -0300

    Update a.py

--- a.py
+++ a.py
@@ -1,9 +1,13 @@
 #
 # criar a class a
 class A:
-  def __init__(self, valor):
-    self._valor = valor
-  def get_valor(self):
+  def __init__(self, valor): # inicializador
+    self._valor = valor # atributo
+
+  # get
+  def get_valor(self): # método de instância
     return self._valor  # Retorna o valor da string  informada
+
+  # set
   def set_valor(self, valor):
     self._valor = valor # Atribui o valor ao atributo
