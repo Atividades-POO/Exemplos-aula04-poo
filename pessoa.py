0a41bfb059ec53680fc1993b30dc45b96e616d6d
Michel <106674127+MichelZero@users.noreply.github.com>
2022-06-24 10:09:37 -0300

    Update pessoa.py

--- pessoa.py
+++ pessoa.py
@@ -6,18 +6,18 @@ class Pessoa:
     self.login = login
     self.logoff = logoff
 
-  def logar(self):
-    if self.login:
-      print(f"{self.nome}, já está logado no sistema!")
-      return
-    print(f"Bem vindo {self.nome}, você logou no sistema.")
-    self.login = True
-    self.logoff = False
+  def logar(self): # método de instância
+    if self.login:  # se estiver logado
+      print(f"{self.nome}, já está logado no sistema!")  # imprime mensagem
+      return # sai do método logar
+    self.login = True  # loga no sistema e imprime mensagem
+    print(f"Bem vindo {self.nome}, você logou no sistema.") # se não estiver logado
+    self.logoff = False # não está logado no sistema
 
-  def sair(self):
-    if not self.login:
-      print(f"{self.nome}, não está logado no sistema!")
-      return
-    print(f"{self.nome}, você saiu do sistema.")
-    self.login = False
-    self.logoff = True
+  def sair(self): # método de instância
+    if not self.login: # se não estiver logado
+      print(f"{self.nome}, não está logado no sistema!") # imprime mensagem e sai do método
+      return # sai do método sair
+    self.login = False  # não está logado no sistema e imprime mensagem
+    print(f"{self.nome}, você saiu do sistema.") # se estiver logado
+    self.logoff = True # está logado no sistema 
