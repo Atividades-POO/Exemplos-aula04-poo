111b702549cd76014df736ecb9aee05cd735619d
Michel <106674127+MichelZero@users.noreply.github.com>
2022-06-24 10:09:46 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ p1 = P('Davi')
 # 1.2 chame o método logar duas vezes seguidas e veja as mensagens de log
 p1.logar()
 p1.logar()
+# 1.3 chame o método sair duas vezes seguidas e veja as mensagens de log
+p1.sair()
+p1.sair()
+
